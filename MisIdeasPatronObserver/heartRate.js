89b25ede86ac8c23976db8088ca82f0d915876c6
Jalexss <jhalx2198@gmail.com>
2021-07-07 07:55:40 -0500

    Fixed heartRate

--- MisIdeasPatronObserver/heartRate.js
+++ MisIdeasPatronObserver/heartRate.js
@@ -2,12 +2,25 @@ const { HeartRate } = require('./observable/observable');
 
 class BeatsOne {
 	notify(event) {
-		console.log(`hearthbeat-1 is: ${event.beat}`);
+		switch (event.beat) {
+			case event.beat < 50:
+				console.log('Warning, bradycardia ');
+				break;
+			case event.beat > 100:
+				console.log('Warning, tachycardia ');
+				break;
+
+			default:
+				console.log('you are healthy!');
+				
+				break;
+		}
 	}
 }
 class BeatsTwo {
 	notify(event) {
-		console.log(`hearthbeat-2 is: ${event.beat}`);
+		if (event.beat > 60 && event.beat < 100)
+			console.log(`hearthbeat-2 is: ${event.beat}`);
 	}
 }
 
@@ -17,6 +30,5 @@ heartRate.subscribe(new BeatsOne());
 heartRate.subscribe(new BeatsTwo());
 
 setInterval(() => {
-    
-    heartRate.heartbeat();
+	heartRate.heartbeat();
 }, 1000);
